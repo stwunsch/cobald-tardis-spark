e944a1d326973d507dbb47f74c9195f18fd3df97
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2021-02-15 12:14:40 +0100

    fix install script for python and add dependencies

--- install_python.sh
+++ install_python.sh
@@ -9,6 +9,6 @@ $PYTHON_BINARY -m venv $PROJECT_HOME/pyvenv
 # Install all the required packages
 source $PROJECT_HOME/pyvenv/bin/activate
 pip install --upgrade pip
-pip install -f $PROJECT_HOME/requirements.txt
+pip install -r $PROJECT_HOME/requirements.txt
 pip install -e $PROJECT_HOME/cobald
 pip install -e $PROJECT_HOME/tardis
