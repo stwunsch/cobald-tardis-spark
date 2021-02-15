8540bf8bb940b10494e62c54eea46c87de3fa2a4
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2021-02-15 11:37:35 +0100

    add install script for spark and hadoop

--- install.sh
+++ install.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-# Import settings like project home, path to python binary, ...
 source config.sh
 
-# Setup python virtual environment
-$PYTHON_BINARY -m venv $PROJECT_HOME/pyvenv
-
-# Install all the required packages
-source $PROJECT_HOME/pyvenv/bin/activate
-pip install --upgrade pip
-pip install -f $PROJECT_HOME/requirements.txt
-pip install -e $PROJECT_HOME/cobald
-pip install -e $PROJECT_HOME/tardis
+$PROJECT_HOME/install_python.sh
+$PROJECT_HOME/install_spark_hadoop.sh
---
+++ install_spark_hadoop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source config.sh
+
+# Spark
+NAME=spark-3.0.1-bin-hadoop3.2
+curl -Os https://downloads.apache.org/spark/spark-3.0.1/$NAME.tgz
+tar xvf $NAME.tgz
+rm $NAME.tgz
+mv $NAME spark/
+
+# Hadoop suite
+NAME=hadoop-3.3.0
+curl -Os https://downloads.apache.org/hadoop/common/$NAME/$NAME.tar.gz
+tar xvf $NAME.tar.gz
+rm $NAME.tar.gz
+mv $NAME hadoop/
