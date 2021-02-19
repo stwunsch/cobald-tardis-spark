6506e1d6089d66deb5532d38791c26e2ccce557b
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2021-02-19 14:35:12 +0100

    set yarn.nodemanager.local-dir to a node local directory (/tmp)

--- configure.sh
+++ configure.sh
@@ -6,7 +6,6 @@ source $SCRIPT_DIR/config.sh
 
 # Hadoop
 sed -i 's,CONFIGURE_YARN_LOG_DIR,'$PROJECT_TMP'/yarn-logs,g' $HADOOP_CONF_DIR/yarn-env.sh
-sed -i 's,CONFIGURE_YARN_LOCAL_DIR,'$PROJECT_TMP'/yarn-local-dir,g' $HADOOP_CONF_DIR/yarn-site.xml
 sed -i 's,CONFIGURE_YARN_RESOURCEMANAGER,'$YARN_RESOURCEMANAGER',g' $HADOOP_CONF_DIR/yarn-site.xml
 
 # Spark
--- tmp/yarn-local-dir/README.md
+++
@@ -1,1 +0,0 @@
-Here goes the local temporary files of Yarn.
