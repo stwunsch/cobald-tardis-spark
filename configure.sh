5fb6173298407e86cd14d3077fc3b609b69c4d00
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2021-02-19 14:45:16 +0100

    also set yarn log directory to /tmp to avoid nameclashes

--- configure.sh
+++ configure.sh
@@ -5,7 +5,6 @@ SCRIPT_DIR=$(dirname $SCRIPT)
 source $SCRIPT_DIR/config.sh
 
 # Hadoop
-sed -i 's,CONFIGURE_YARN_LOG_DIR,'$PROJECT_TMP'/yarn-logs,g' $HADOOP_CONF_DIR/yarn-env.sh
 sed -i 's,CONFIGURE_YARN_RESOURCEMANAGER,'$YARN_RESOURCEMANAGER',g' $HADOOP_CONF_DIR/yarn-site.xml
 
 # Spark
