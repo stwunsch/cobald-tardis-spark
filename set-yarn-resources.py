07718b22d18c7d510a0ba440ae3d110808c12bf3
Vincenzo Eduardo Padulano <v.e.padulano@gmail.com>
2021-02-25 12:23:05 +0100

    Add command line arguments to change yarn resources in python script

--- set-yarn-resources.py
+++ set-yarn-resources.py
@@ -1,5 +1,13 @@
 import requests
 
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--cores", type=int, help="Number of cores to allocate on the nodes.")
+parser.add_argument("--memory", type=int, help="Amount of memory (MB) to allocate on the nodes.")
+parser.add_argument("--nodes", nargs="+", help="Space separated list of nodes which resources should be changed.")
+args = parser.parse_args()
+
 class YarnResourceManager:
     def __init__(self, hostname, ip=8088):
         self.hostname = hostname
@@ -29,7 +37,25 @@ class YarnResourceManager:
         r = requests.post(self.base + '/ws/v1/cluster/nodes/' + node + '/resource', json=payload)
 
 rm = YarnResourceManager('portal1')
-nodes = rm.nodes
-for node in nodes:
-    print(f'Set resources for node {node}')
-    rm.set_resources(node, cores=4, memory=5000)
+cluster_nodes = rm.nodes
+
+thiscores = args.cores if args.cores else 4
+thismemory = args.memory if args.memory else 5000
+
+if args.nodes:
+    for nodename in args.nodes:
+        print(f'Selected node name {nodename}')
+        name_in_address = [nodename in fulladdress for fulladdress in cluster_nodes]
+        if any(name_in_address):
+            node = cluster_nodes[name_in_address.index(True)]
+            print(f'Set resources for node {node}: ')
+            rm.set_resources(node, cores=thiscores, memory=thismemory)
+            print('Node resources: ', rm.get_resources(node))
+        else:
+            print('Selected node name is not available')
+            print(f'Available nodes {cluster_nodes}')
+else:
+    for node in cluster_nodes:
+        print(f'Set resources for node {node}: ')
+        rm.set_resources(node, cores=thiscores, memory=thismemory)
+        print('Node resources: ', rm.get_resources(node))
