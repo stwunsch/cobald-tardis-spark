73a91bbc5dab3301c876ffc4dcc0619adaf5bc1d
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2021-03-04 09:46:38 +0100

    add script to run nodemanagers on various machines

---
+++ start-nodemanagers.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+SCRIPT=$(readlink -f $BASH_SOURCE[0])
+SCRIPT_DIR=$(dirname $SCRIPT)
+
+MODE=$1
+
+HOSTNAMES=(
+    sg01.etp.kit.edu
+    sg02.etp.kit.edu
+    sg03.etp.kit.edu
+    sg04.etp.kit.edu
+)
+
+USER=$(whoami)
+
+for HOSTNAME in ${HOSTNAMES[@]}
+do
+    if [ ${MODE} = "start" ]
+    then
+        echo "Start nodemanager on" ${HOSTNAME}
+        ssh ${USER}@${HOSTNAME} "screen -d -m ${SCRIPT_DIR}/run-nodemanager.sh"
+    elif  [ ${MODE} = "status" ]
+    then
+        echo "Show screen sessions on" ${HOSTNAME}
+        ssh ${USER}@${HOSTNAME} "screen -ls"
+    elif  [ ${MODE} = "stop" ]
+    then
+        echo "Kill screen sessions on" ${HOSTNAME}
+        ssh ${USER}@${HOSTNAME} "killall screen"
+    else
+        echo "Unknown mode: " ${MODE}
+        exit
+    fi
+done
